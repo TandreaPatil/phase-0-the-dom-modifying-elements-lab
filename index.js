2248978c8878cacd88fed190e616bbc7d93ae57c
tandrea patil <tandreayoung@gmail.com>
2022-07-20 15:44:28 -0500

    commit tree d3fe4bd92428f84d1b8c5e8af39e345d2008f434

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var newHeader = document.querySelector('H1#victory');
+//document.getElementById('main').remove();
+//const newHeader = document.createElement("h1")
+////document.body.append(newHeader);
+//h1.innerHTML = 
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.setAttribute("h1", "victory");
+
+newHeader.textContent = "Tandrea is the champion"
